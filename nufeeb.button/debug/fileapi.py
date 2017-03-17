2e98daeff0f5eb3afb3679c3baf13863fb32be7a
LimXS <xsx2018@yahoo.com>
2017-03-17 15:52:49 +0800

    20170317

--- nufeeb.button/debug/fileapi.py
+++ nufeeb.button/debug/fileapi.py
@@ -7,6 +7,7 @@ browser=browserClass.browser()
 f=open('e:src.txt','r')
 l=f.read()
 print l
+headers={'cookie':l}
 #cookie='_ati=146267075305; ASP.NET_SessionId=qe5bebuz2cphgb0o01omllql; _umdata=65F7F3A2F63DF0207E2BEE3A391EB24C36974786A7AE62C01730E5CD202421301CEB2174FC2A3B8ECD43AD3E795C914C875265D2A3D8556E4D50D098EE48453E; corpName=%E6%B5%8B%E8%AF%95%E6%AF%95%E6%96%B9%E6%A1%82; userName=%E6%AF%95%E6%96%B9; Beefun.LoginValidation.ValidationHandler.WhiteListHandler=09:53:10 276; BeefunMainOpenMark=2017/3/17%20%u4E0A%u53489%3A53%3A34540; quickDate=4; MDIPageLocation=%7E%252FBeefun%252FBaseInfo2%252FPTypeList.gspx'
 '''
 url="http://beefun.wsgjp.com/Carpa.Web/Carpa.Web.Script.DataService.ajax/GetPagerData"
@@ -25,7 +26,7 @@ url="http://beefun.wsgjp.com/Beefun/Beefun.IniPeriod.ImportKtypeIniNew.ajax/Save
 files={'_dataField':(None,'%7B%22ktypeid%22%3A%222605637371178862817%22%2C%22stockid%22%3A%222605637371178862817%22%2C%22kfullname%22%3A%22%u4E3B%u4ED3%u5E93%22%2C%22importertype%22%3A%22jxc%22%7D'),
   'loadfile':('abc.xls',open('D:abc.xls','rb'),'application/vnd.ms-excel')
  }
-headers={'cookie':l}
+
 response=requests.post(url,files=files,headers=headers)
 print response.text
 
