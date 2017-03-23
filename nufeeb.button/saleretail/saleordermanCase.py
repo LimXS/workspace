f889f12569839dc0dfbd970b36a11f7afbd28413
LimXS <xsx2018@yahoo.com>
2017-03-23 14:21:06 +0800

    20170323

--- nufeeb.button/saleretail/saleordermanCase.py
+++ nufeeb.button/saleretail/saleordermanCase.py
@@ -26,7 +26,7 @@ class saleordermanTest(unittest.TestCase):
 
     def tearDown(self):
         print "test over"
-        #self.driver.close()
+        self.driver.close()
         pass
 
     def testnewsaleorderMan(self):
